4ba84ce4e57e2f39e93b31476a891930c8ebd01f
Raghad Al Akhras <raghad412102@gmail.com>
2026-07-10 18:46:15 +0300

    Fix script link and update theme settings

---
+++ js/settings.js
@@ -0,0 +1,18 @@
+const themeToggleBtn = document.getElementById('toggleBtn');
+const htmlElement = document.documentElement;
+
+if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    htmlElement.classList.add('dark');
+} else {
+    htmlElement.classList.remove('dark');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    htmlElement.classList.toggle('dark');
+    
+    if (htmlElement.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
